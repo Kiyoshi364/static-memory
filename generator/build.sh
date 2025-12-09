bc9af29da1532c6143d789adb12b515acaf95244
Shikiyo <shikiyo@archjk>
2025-12-09 15:13:14 -0300

    Use exec in build.sh

--- generator/build.sh
+++ generator/build.sh
@@ -5,6 +5,5 @@ set -xe
 FILE="main.pl"
 DIR="$PWD"
 
-pushd ..
-scryer-prolog --no-add-history -f -g main -g halt "${DIR}/${FILE}"
-popd
+cd ..
+exec scryer-prolog --no-add-history -f -g main -g halt "${DIR}/${FILE}"
